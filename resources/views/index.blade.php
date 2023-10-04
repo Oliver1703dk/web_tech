082a654dce5d5c96a4d59f1629babf6bed5a50d6
Kristoffer <Kripe22@student.sdu.dk>
2023-10-04 13:41:19 +0200

    icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,17 +4,17 @@
 <head>
 <title>Hej</title>
 <script src="https://cdn.tailwindcss.com"></script>
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/mail.css' rel='stylesheet'>
 </head>
 <body>
-<div class="bg-[url('images/BNG.png')]">
-<div class="w-96 h-96 relative">
-    <div class="w-96 h-96 left-[414px] top-[205px] absolute">
-        <div class="w-96 h-96 left-0 top-0 absolute bg-neutral-900 rounded-2xl shadow"></div>
-        <div class="w-96 h-12 left-[38px] top-[99px] absolute bg-white rounded-2xl shadow"></div>
-        <div class="left-[38px] top-[27px] absolute text-white text-5xl font-normal font-['Krona One']">E-mail</div>
-        <div class="w-96 h-12 left-[42px] top-[237px] absolute bg-white rounded-2xl shadow"></div>
+<div class="w-96 h-full relative">
+    <div class="w-96 h-full left-[414px] top-[205px] absolute">
+        <div class="w-[30rem] h-[43rem] left-0 top-0 absolute bg-blue-300 rounded-2xl shadow"></div>
+        <div class="w-80 h-12 left-[38px] top-[99px] absolute bg-white rounded-2xl shadow"></div>
+        <div class="left-[38px] top-[27px] absolute text-white text-5xl font-normal font-['Krona One']">E-mail<i class="gg-mail"></i></div>
+        <div class="w-80 h-12 left-[42px] top-[237px] absolute bg-white rounded-2xl shadow"></div>
         <div class="left-[42px] top-[165px] absolute text-white text-5xl font-normal font-['Krona One']">Password</div>
-        <div class="w-96 h-12 left-[43px] top-[375px] absolute bg-white rounded-2xl shadow"></div>
+        <div class="w-80 h-12 left-[43px] top-[375px] absolute bg-white rounded-2xl shadow"></div>
         <div class="left-[43px] top-[307px] absolute text-white text-4xl font-normal font-['Krona One']">Repeat password</div>
         <div class="w-96 h-12 left-[42px] top-[516px] absolute bg-white rounded-2xl shadow"></div>
         <div class="left-[42px] top-[444px] absolute text-white text-5xl font-normal font-['Krona One']">Phone Number</div>
@@ -24,8 +24,7 @@
             <div class="left-[13px] top-[3px] absolute text-white text-5xl font-normal font-['Krona One']">Create Account</div>
         </div>
     </div>
-    <div class="left-[567px] top-[99px] absolute text-white text-5xl font-normal font-['Krona One']">Welcome</div>
-</div>
+    <div class="left-[567px] top-[99px] absolute text-black text-5xl font-normal font-['Krona One']">Welcome</div>
 </div>
 </body>
 
