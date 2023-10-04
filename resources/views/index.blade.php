6fb20211c82a6a6fa70a3ba0ecc8d262c2e356a4
Kristoffer <Kripe22@student.sdu.dk>
2023-10-04 11:35:16 +0200

    Added tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,29 +6,24 @@
 
 </head>
 <body>
-<div style="width: 100%; height: 100%; position: relative; background: linear-gradient(0deg, #E5E5E5 0%, #E5E5E5 100%); background-image: url(https://via.placeholder.com/1440x1024)">
-    <div style="width: 612px; height: 714px; left: 414px; top: 205px; position: absolute">
-        <div style="width: 612px; height: 714px; left: 0px; top: 0px; position: absolute; background: #181818; box-shadow: 4px 4px 4px 2px black; border-radius: 20px"></div>
-        <div style="width: 512px; height: 49px; left: 38px; top: 99px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 20px"></div>
-        <div style="width: 66.67px; height: 43.33px; left: 497px; top: 35px; position: absolute; background: white"></div>
-        <div style="left: 38px; top: 27px; position: absolute; color: white; font-size: 48px; font-family: Krona One; font-weight: 400; word-wrap: break-word">E-mail</div>
-        <div style="width: 512px; height: 49px; left: 42px; top: 237px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 20px"></div>
-        <div style="left: 42px; top: 165px; position: absolute; color: white; font-size: 48px; font-family: Krona One; font-weight: 400; word-wrap: break-word">Password</div>
-        <div style="width: 48px; height: 59px; left: 510px; top: 166px; position: absolute; background: white"></div>
-        <div style="width: 512px; height: 49px; left: 43px; top: 375px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 20px"></div>
-        <div style="left: 43px; top: 307px; position: absolute; color: white; font-size: 40px; font-family: Krona One; font-weight: 400; word-wrap: break-word">Repeat password</div>
-        <div style="width: 59px; height: 38.25px; left: 504px; top: 457.38px; position: absolute; background: white"></div>
-        <div style="width: 512px; height: 49px; left: 42px; top: 516px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 20px"></div>
-        <div style="left: 42px; top: 444px; position: absolute; color: white; font-size: 44px; font-family: Krona One; font-weight: 400; word-wrap: break-word">Phone Number</div>
-        <div style="width: 72px; height: 71px; left: 498px; top: 294px; position: absolute">
-            <div style="width: 48px; height: 62.12px; left: 12px; top: 5px; position: absolute; background: white"></div>
-        </div>
-        <div style="width: 465px; height: 62px; left: 69px; top: 605px; position: absolute">
-            <div style="width: 465px; height: 62px; left: 0px; top: 0px; position: absolute; background: #FCA311; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 20px"></div>
-            <div style="left: 13px; top: 3px; position: absolute; color: white; font-size: 44px; font-family: Krona One; font-weight: 400; word-wrap: break-word">Create Account</div>
+<div class="w-96 h-96 relative">
+    <div class="w-96 h-96 left-[414px] top-[205px] absolute">
+        <div class="w-96 h-96 left-0 top-0 absolute bg-neutral-900 rounded-2xl shadow"></div>
+        <div class="w-96 h-12 left-[38px] top-[99px] absolute bg-white rounded-2xl shadow"></div>
+        <div class="left-[38px] top-[27px] absolute text-white text-5xl font-normal font-['Krona One']">E-mail</div>
+        <div class="w-96 h-12 left-[42px] top-[237px] absolute bg-white rounded-2xl shadow"></div>
+        <div class="left-[42px] top-[165px] absolute text-white text-5xl font-normal font-['Krona One']">Password</div>
+        <div class="w-96 h-12 left-[43px] top-[375px] absolute bg-white rounded-2xl shadow"></div>
+        <div class="left-[43px] top-[307px] absolute text-white text-4xl font-normal font-['Krona One']">Repeat password</div>
+        <div class="w-96 h-12 left-[42px] top-[516px] absolute bg-white rounded-2xl shadow"></div>
+        <div class="left-[42px] top-[444px] absolute text-white text-5xl font-normal font-['Krona One']">Phone Number</div>
+        <div class="w-16 h-16 left-[498px] top-[294px] absolute"></div>
+        <div class="w-96 h-16 left-[69px] top-[605px] absolute">
+            <div class="w-96 h-16 left-0 top-0 absolute bg-amber-500 rounded-2xl shadow"></div>
+            <div class="left-[13px] top-[3px] absolute text-white text-5xl font-normal font-['Krona One']">Create Account</div>
         </div>
     </div>
-    <div style="left: 567px; top: 99px; position: absolute; color: white; font-size: 48px; font-family: Krona One; font-weight: 400; word-wrap: break-word">Welcome</div>
+    <div class="left-[567px] top-[99px] absolute text-white text-5xl font-normal font-['Krona One']">Welcome</div>
 </div>
 
 </body>
