2c342516f5f278488c2083c6c5ba953c18da3577
Kristoffer <Kripe22@student.sdu.dk>
2023-10-11 11:32:04 +0200

    Added DB table TEST Added BG_Img Added env

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,11 @@
     <link href='https://unpkg.com/css.gg@2.0.0/icons/css/mail.css' rel='stylesheet'>
 </head>
 <body>
-<div class="w-96 h-full relative">
+<div class="w-96 h-full relative bg-cyan-950">
     <div class="w-96 h-full left-[414px] top-[205px] absolute">
         <div class="w-[30rem] h-[43rem] left-0 top-0 absolute bg-blue-300 rounded-2xl shadow"></div>
         <div class="w-80 h-12 left-[38px] top-[99px] absolute bg-white rounded-2xl shadow"></div>
-        <div class="left-[38px] top-[27px] absolute text-white text-5xl font-normal font-['Krona One']">E-mail<i class="gg-mail"></i></div>
+        <div class="left-[38px] top-[27px] absolute text-white text-5xl font-normal font-['Krona One']">E-mail
         <div class="w-80 h-12 left-[42px] top-[237px] absolute bg-white rounded-2xl shadow"></div>
         <div class="left-[42px] top-[165px] absolute text-white text-5xl font-normal font-['Krona One']">Password</div>
         <div class="w-80 h-12 left-[43px] top-[375px] absolute bg-white rounded-2xl shadow"></div>
@@ -20,11 +20,16 @@
         <div class="left-[42px] top-[444px] absolute text-white text-5xl font-normal font-['Krona One']">Phone Number</div>
         <div class="w-16 h-16 left-[498px] top-[294px] absolute"></div>
         <div class="w-96 h-16 left-[69px] top-[605px] absolute">
-            <div class="w-96 h-16 left-0 top-0 absolute bg-amber-500 rounded-2xl shadow"></div>
-            <div class="left-[13px] top-[3px] absolute text-white text-5xl font-normal font-['Krona One']">Create Account</div>
+            <div class="w-96 h-16 left-0 top-0 absolute bg-amber-500 rounded-2xl shadow hover:bg-amber-200"></div>
+            <div class="left-[13px] top-[3px] absolute text-white text-5xl font-normal font-['Krona One'] ">Create Account</div>
         </div>
     </div>
-    <div class="left-[567px] top-[99px] absolute text-black text-5xl font-normal font-['Krona One']">Welcome</div>
+    <div class="left-[567px] top-[99px] absolute text-black text-5xl font-normal font-['Krona One']">Welcome
+    <svg xmlns="http://www.w3.org/2000/svg" fill="2" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 fill-amber-400">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+    </svg>
+    </div>
+</div>
 </div>
 </body>
 
