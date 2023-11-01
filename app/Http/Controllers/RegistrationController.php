d3d0a2b8a84af7de791da15aad4cceab93213ae9
Oliver Larsen <Oliver1703dk@hotmail.com>
2023-11-01 12:34:27 +0100

    test

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 class RegistrationController {
     public function register() {
-        // Handle user registration
+        // Handle user registrationn
     }
 }
 
