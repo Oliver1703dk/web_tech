1cad9f4f7042cbb40deccc22eab19c9c174f9698
KennyLVN <kenny2011@live.dk>
2023-11-08 10:29:25 +0100

    payment 50%

---
+++ resources/views/payment.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <title>Index</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <body>
+    <span>Customer Information</span>
+    <div class = "w-1/2">
+        <div class="grid grid-cols-2 gap-4">
+            <input type="text" name="firstName"
+                   class="border rounded h-10 w-full focus:outline-none"
+                   placeholder="First name*">
+
+            <input type="text" name="lastName"
+                   class="border rounded h-10 w-full focus:outline-none "
+                   placeholder="Last name*">
+        </div>
+
+             <div class="grid grid-cols-1 gap-4">
+                <input type="text" name="mail"
+                        class="border rounded h-10 w-full focus: outline-none"
+                         placeholder="Adress*">
+                 <input type="text" name="mail"
+                        class="border rounded h-10 w-full focus: outline-none"
+                        placeholder="Apartment* (optional)">
+        </div>
+    <div class="grid grid-cols-2 gap-4">
+        <input type="text" name="firstName"
+               class="border rounded h-10 w-full focus:outline-none"
+               placeholder="Zipcode*">
+
+        <input type="text" name="lastName"
+               class="border rounded h-10 w-full focus:outline-none "
+               placeholder="City*">
+    </div>
+    <div class="grid grid-cols-1 gap-4">
+        <input type="text" name="mail"
+               class="border rounded h-10 w-full focus: outline-none"
+               placeholder="Phone Number">
+    </div>
+    </div>
+
+    </body>
+<html>
