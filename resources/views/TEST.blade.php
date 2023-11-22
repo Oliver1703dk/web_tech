5b3eafd23c9a053d71df79865fda165820f4c784
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2023-11-22 09:50:12 +0100

    login page input bar fixes, index right page

--- resources/views/TEST.blade.php
+++ resources/views/TEST.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Computer Store</title>
+    <title>product page</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
@@ -19,16 +19,21 @@
                 <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div class="relative">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" id="default-search" class="block w-[40rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
-                    <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                    <input type="search" id="default-search"
+                           class="block w-[40rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="Search Mockups, Logos..." required>
+                    <button type="submit"
+                            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Search
+                    </button>
                 </div>
             </form>
-
-
 
 
             <div class="flex items-center lg:order-2">
@@ -51,50 +56,20 @@
 
 <div>
     <div class="h-[91px] bg-amber-500"></div>
-    <img class="h-[460px]" src="https://via.placeholder.com/1440x460"/>
-    <div class="h-48 bg-amber-500"></div>
-    <div class="bg-[#141414]">
-        <section class="container mx-auto p-4">
-            <div class="grid grid-cols-7 gap-4">
-                <article class="bg-white shadow-md p-4">
-                    <img src="../Pictures/product1.jpg" alt="Product 1">
-                    <h2 class="text-lg font-semibold">Product 1</h2>
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded">Add to Cart</button>
-                </article>
-                <article class="bg-white shadow-md p-4">
-                    <img src="../Pictures/product2.jpg" alt="Product 2">
-                    <h2 class="text-lg font-semibold">Product 2</h2>
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded">Add to Cart</button>
-                </article>
-                <!-- Add more product cards here -->
-                <article class="bg-white shadow-md p-4">
-                    <img src="../Pictures/product1.jpg" alt="Product 1">
-                    <h2 class="text-lg font-semibold">Product 1</h2>
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded">Add to Cart</button>
-                </article>
-                <article class="bg-white shadow-md p-4">
-                    <img src="../Pictures/product2.jpg" alt="Product 2">
-                    <h2 class="text-lg font-semibold">Product 2</h2>
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded">Add to Cart</button>
-                </article>
-                <article class="bg-white shadow-md p-4">
-                    <img src="../Pictures/product1.jpg" alt="Product 1">
-                    <h2 class="text-lg font-semibold">Product 1</h2>
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded">Add to Cart</button>
-                </article>
-                <article class="bg-white shadow-md p-4">
-                    <img src="../Pictures/product2.jpg" alt="Product 2">
-                    <h2 class="text-lg font-semibold">Product 2</h2>
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded">Add to Cart</button>
-                </article>
-                <article class="bg-white shadow-md p-4">
-                    <img src="../Pictures/product1.jpg" alt="Product 1">
-                    <h2 class="text-lg font-semibold">Product 1</h2>
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded">Add to Cart</button>
-                </article>
-            </div>
-        </section>
+
+    <div class="grid grid-cols-2 gap-4">
+        <div>
+            <img class="h-[460px]" src="https://via.placeholder.com/1440x460" alt="Error"/>
+        </div>
+        <div>
+            <p class="text-xl font-bold">Product title</p>
+            <br>
+            <p class="text-xl font-bold">Price</p>
+            <br>
+            <p class="text-xl font-bold border-b-2 border-amber-500 mb-4 w-2/5">Stock status</p>
+        </div>
     </div>
+
 </div>
 
 <footer class="bg-[#141414] text-white p-4">
