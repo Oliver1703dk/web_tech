a65235489eaa45fdffa69719804d3a89342224d9
Oliver Larsen <Oliver1703dk@hotmail.com>
2023-12-13 21:26:56 +0100

    Cart allmost works fully. Only need taxes

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -3,78 +3,67 @@
 @section("title", "cart")
 
 @section("content")
-<div>
-    <h2 class="text-lg font-bold mb-3">Cart: <span id="cart-count">0</span></h2>
-</div>
 
-<!-- Cart Items Section -->
-<div class="flex-1 px-2">
-    <h2 class="text-lg font-bold mb-3">Cart</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"></div>
+    <div class="grid grid-cols-2 gap-4 bg-amber-500">
 
+        <!-- User's products Section -->
+        <div>
+            <div class="grid grid-cols-2 gap-4 m-10">
+                <!-- Produkter nedenunder er ikke dem der er i users cart-->
+                @php $subtotal = 0; @endphp
+                @foreach ($cartItems as $productList)
+                    @php $subtotal = $subtotal + $productList['product']->price * $productList['quantity']; @endphp
+                    @include("productBoxCartPage", [
+                        "Product" => $productList
+                    ])
+                @endforeach
+{{--                @include("productBox")--}}
+{{--                @include("productBox")--}}
+{{--                @include("productBox")--}}
+{{--                @include("productBox")--}}
 
+            </div>
+        </div>
 
+        <!-- Cart Items Section -->
+        <div class="m-10">
+            <div class="bg-white rounded-lg shadow-md p-6">
+                <h2 class="text-lg font-semibold mb-4">Summary</h2>
+                <div class="flex justify-between mb-2">
+                    <span>Subtotal</span>
+                    <span>${{ number_format($subtotal, 2) }}</span>
+                </div>
+                <div class="flex justify-between mb-2">
+                    <span>Taxes</span>
+                    <span>$1.99</span>
+                </div>
+                <div class="flex justify-between mb-2">
+                    <span>Shipping</span>
+                    <span>$0.00</span>
+                </div>
+                <hr class="my-2">
+                <div class="flex justify-between mb-2">
+                    <span class="font-semibold">Total</span>
 
-            <div class="w-full lg:w-1/2 p-5">
+                    <span class="font-semibold">${{ number_format($subtotal, 2) }}</span>
+                </div>
+                <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+            </div>
 
-                <div class="flex flex-col md:flex-row">
-                    <h2 class="text-lg font-bold mb-3">Cart</h2>
-                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                        @foreach ($cartItems as $item)
-                            <div class="border p-4">
-                                <h3 class="text-lg font-bold">{{ $item['product']->name }}</h3>
-{{--                                <p>{{ $item['product']->description }}</p>--}}
-                                <p>Price: ${{ $item['product']->price }}</p>
-                                <p>Quantity: {{ $item['quantity'] }}</p>
-                                <!-- Add more item details as needed -->
-                            </div>
-                        @endforeach
-                    </div>
-
-
-
-                    <div id="summaryBox" class="max-w-sm lg:mx-0 mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-                    <div class="p-5">
-                        <h3 class="text-lg font-semibold">Summary</h3>
-                        <div class="space-y-2 mt-3">
-                            <div class="flex justify-between">
-                                <span>Subtotal:</span>
-                                <span id="subtotalPrice">-</span>
-                            </div>
-                            <div class="flex justify-between">
-                                <span>Tax:</span>
-                                <span id="taxPrice">-</span>
-                            </div>
-                            <div class="flex justify-between">
-                                <span>Delivery:</span>
-                                <span id="deliveryPrice">-</span>
-                            </div>
-                            <div class="border-t border-gray-200 pt-2 mt-2">
-                                <div class="flex justify-between font-semibold">
-                                    <span>Total:</span>
-                                    <span id="totalPrice">-</span>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="mt-4 w-full bg-yellow-500 text-white p-2 rounded hover:bg-yellow-600 transition-colors duration-200">
-                            Discount or gift card
-                        </button>
-                    </div>
+            <!-- Deals of the Week Section -->
+            <div class="mt-10">
+                <div>
+                    <p class="text-xl font-bold">
+                        Great Deals of the week:
+                    </p>
+                </div>
+                <div class="grid grid-cols-2 gap-4">
+                    <!-- Sørger for der kun kan komme 2 productBox på den her side-->
+                    @include("productBox")
+                    @include("productBox")
                 </div>
             </div>
         </div>
-    <!-- Deals of the Week Section -->
-    <div class="px-2 mt-5">
-        <h2 class="text-lg font-bold mb-3">Great Deals of the Week</h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            <!-- Example deal item -->
-            <div class="bg-black rounded-lg p-4 flex items-center justify-center">
-                <!-- Image -->
-                <img class="h-24" src="resources/Pictures/product2.jpg" alt="Deal Product">
-            </div>
-            <!-- Repeat for each deal item... -->
-        </div>
+
     </div>
 @endsection
-
-
--- resources/views/productBoxCartPage.blade.php
+++ resources/views/productBoxCartPage.blade.php
@@ -1,18 +1,28 @@
+{{--<article class="bg-white shadow-md p-4">--}}
+{{--    <div class="border p-4">--}}
+{{--        <h3 class="text-lg font-bold">{{ $item['product']->name }}</h3>--}}
+{{--        --}}{{--                                <p>{{ $item['product']->description }}</p>--}}
+{{--        <p>Price: ${{ $item['product']->price }}</p>--}}
+{{--        <p>Quantity: {{ $item['quantity'] }}</p>--}}
+{{--        <!-- Add more item details as needed -->--}}
+{{--    </div>--}}
+{{--</article>--}}
+
 <article class="bg-white shadow-md p-4">
-    <div class="border p-4">
-        <h3 class="text-lg font-bold">{{ $item['product']->name }}</h3>
-        {{--                                <p>{{ $item['product']->description }}</p>--}}
-        <p>Price: ${{ $item['product']->price }}</p>
-        <p>Quantity: {{ $item['quantity'] }}</p>
-        <!-- Add more item details as needed -->
-    </div>
+    <img src="https://static.thenounproject.com/png/1226185-200.png">
+    <h2 class="text-lg font-semibold">{{ $Product['product']->name }}</h2>
+    <p>Price: ${{ $Product['product']->price }}</p>
+    <p>Quantity: {{ $Product['quantity'] }}</p>
+    <button class="bg-blue-500 text-white py-2 px-4 rounded">Click for More</button>
 </article>
 
-@foreach ($products['product'] as $product)
-    @include("productBox1", [
-        "Product" => $product
-    ])
-@endforeach
+
+
+{{--@foreach ($products['product'] as $product)--}}
+{{--    @include("productBoxCartPage", [--}}
+{{--        "Product" => $product--}}
+{{--    ])--}}
+{{--@endforeach--}}
 
 
 
