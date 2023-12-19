ef8f912a9a3880cd3af307e3260024c205bba654
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2023-12-19 19:18:18 +0100

    css and placement on paymentComplete

--- resources/views/paymentComplete.blade.php
+++ resources/views/paymentComplete.blade.php
@@ -4,17 +4,20 @@
 
 @section("content")
 
-<div class="flex items-center lg:order-2">
-<p class ="no-underline" > Payment Succes </p>
-</div>
+    <div class="h-screen m-10">
+        <div class="flex items-center lg:order-2">
+            <p class="no-underline text-4xl">
+                Payment Succes </p>
+        </div>
 
-<div>
-    <img src="https://cdn.iconscout.com/icon/free/png-256/free-verify-perfect-heavy-check-mark-38018.png">
-</div>
+        <div class="m-10">
+            <img src="https://cdn.iconscout.com/icon/free/png-256/free-verify-perfect-heavy-check-mark-38018.png">
+        </div>
 
-<div>
-    <p class ="no-underline"> Thank you for shopping at All your need pc</p>
-</div>
-
+        <div>
+            <p class="no-underline text-4xl">
+                Thank you for shopping at All your need pc</p>
+        </div>
+    </div>
 
 @endsection
